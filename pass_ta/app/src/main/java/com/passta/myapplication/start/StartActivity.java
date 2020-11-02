6963c1f1dfe93b2184443be127bcfdb2e5df032a
mo_ah <mo_ah@naver.com>
2020-11-02 16:53:56 +0900

    second commit

---
+++ pass_ta/app/src/main/java/com/passta/myapplication/start/StartActivity.java
@@ -0,0 +1,16 @@
+package com.passta.myapplication.start;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.passta.myapplication.R;
+
+public class StartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+    }
+}
